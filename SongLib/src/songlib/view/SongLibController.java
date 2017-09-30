caea9d7f4fc72ab49ed62ba1e2c52802ec873584
rookaj <arook8@gmail.com>
2017-09-30 11:54:53 -0400

    First Commit

---
+++ SongLib/src/songlib/view/SongLibController.java
@@ -0,0 +1,19 @@
+package songlib.view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class SongLibController {
+
+	@FXML Button f2c;
+	@FXML Button c2f;
+	@FXML TextField f;
+	@FXML TextField c;
+	
+	public void convert(ActionEvent e) {
+		
+	}
+	
+}
