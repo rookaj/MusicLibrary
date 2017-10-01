f082e4df73e1360892d0d471127f3b4a396aadac
rookaj <arook8@gmail.com>
2017-09-30 20:49:33 -0400

    App runs and the song library gets pre-loaded.

--- SongLib/src/songlib/app/SongLibApp.java
+++ SongLib/src/songlib/app/SongLibApp.java
@@ -1,30 +1,33 @@
 package songlib.app;
-	
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import songlib.view.SongLibController;
 
 public class SongLibApp extends Application {
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		
-		FXMLLoader loader = new FXMLLoader();
-		loader.setLocation(getClass().getResource("/songlib/view/SongLib.fxml"));
-		
-		GridPane root = (GridPane)loader.load();
-		
-		Scene scene = new Scene(root);
-		primaryStage.setScene(scene);
-		primaryStage.setTitle("Song Library");
-		primaryStage.setResizable(false);  
-		primaryStage.show();
-	}
+    @Override public void start(Stage primaryStage) throws Exception {
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/songlib/view/SongLib.fxml"));
+
+        AnchorPane root = (AnchorPane) loader.load();
+
+        SongLibController songLibController = loader.getController();
+        songLibController.start();
+
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Song Library");
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 
 }
--- SongLib/src/songlib/view/SongLibController.java
+++ SongLib/src/songlib/view/SongLibController.java
@@ -1,19 +1,79 @@
 package songlib.view;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.control.ListView;
 
 public class SongLibController {
 
-	@FXML Button f2c;
-	@FXML Button c2f;
-	@FXML TextField f;
-	@FXML TextField c;
-	
-	public void convert(ActionEvent e) {
-		
-	}
-	
+    @FXML Button add;
+    @FXML Button edit;
+    @FXML Button delete;
+    @FXML ListView<String> songs;
+    @FXML ListView<String> artists;
+
+    private ObservableList<String> songList;
+    private ObservableList<String> artistList;
+    private ArrayList<String> detailedList;
+    
+    public void start() {
+
+        try {
+            ArrayList<String> songArray = new ArrayList<String>();
+            ArrayList<String> artistArray = new ArrayList<String>();
+            detailedList = getLibrary();
+            for(String item: detailedList) {
+                songArray.add(item.split(":")[0]);
+                artistArray.add(item.split(":")[1]);
+            }
+            
+            songList = FXCollections.observableArrayList(songArray);
+            artistList = FXCollections.observableArrayList(artistArray);
+            songs.setItems(songList);
+            artists.setItems(artistList);
+        } catch (FileNotFoundException e) {
+            System.out.println("Library Data File not found.");
+            System.err.println(e);
+            System.exit(0);
+        }
+    }
+
+    private ArrayList<String> getLibrary() throws FileNotFoundException {
+        
+        ArrayList<String> libraryData = new ArrayList<String>();
+        Scanner libraryFile = new Scanner(new File("SongLibrary.txt"));
+
+        while(libraryFile.hasNextLine()) {
+            String line = libraryFile.nextLine();
+            Scanner scanner = new Scanner(line);
+            scanner.useDelimiter(",");
+            while(scanner.hasNext()) {
+                libraryData.add(scanner.next());
+            }
+            scanner.close();
+        }
+        
+        libraryFile.close();
+        return libraryData;
+    }
+
+    public void addSong(ActionEvent e) {
+
+    }
+
+    public void editSong(ActionEvent e) {
+
+    }
+
+    public void deleteSong(ActionEvent e) {
+
+    }
+
 }
